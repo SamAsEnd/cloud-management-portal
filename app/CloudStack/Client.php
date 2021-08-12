a2bf3b1036dfd7986aa4beb1feb2215852ebb94b
Samson Endale <4sam21@gmail.com>
2021-08-12 23:21:27 +0300

    Client execute command implemented

--- app/CloudStack/Client.php
+++ app/CloudStack/Client.php
@@ -2,9 +2,12 @@
 
 namespace App\CloudStack;
 
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
+
 class Client
 {
-
     protected string $endpoint;
 
     protected string $key;
@@ -31,5 +34,44 @@ class Client
     public function getSecret()
     {
         return $this->secret;
+    }
+
+    /**
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public function exec(string $command, array $parameters = []): Response
+    {
+        $queryParams = $this->buildQueryParameters($command, $parameters);
+
+        $queryString = http_build_query($queryParams);
+
+        $queryParams['signature'] = $this->buildSignature($queryString);
+
+        return Http::get($this->endpoint, $queryParams)->throw();
+    }
+
+    private function buildQueryParameters(string $command, array $parameters = null): array
+    {
+        $additionalParameters = [
+            'response' => 'json',
+            'command' => $command,
+            'apiKey' => $this->key,
+        ];
+
+        return collect($parameters + $additionalParameters)
+            ->mapWithKeys(fn($value, $key) => [Str::lower($key) => rawurlencode($value)])
+            ->sortKeys()
+            ->toArray();
+    }
+
+    private function buildSignature(string $queryString): string
+    {
+        $lowered = Str::lower($queryString);
+
+        $signature = hash_hmac('sha1', $lowered, $this->secret, true);
+
+        $based64Encoded = base64_encode($signature);
+
+        return rawurldecode($based64Encoded);
     }
 }
--- tests/Feature/CloudStack/ClientTest.php
+++ tests/Feature/CloudStack/ClientTest.php
@@ -4,8 +4,9 @@ namespace Tests\Feature\CloudStack;
 
 use App\CloudStack\Client;
 use Illuminate\Contracts\Container\BindingResolutionException;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Client\Request;
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class ClientTest extends TestCase
@@ -23,10 +24,41 @@ class ClientTest extends TestCase
 
     public function test_client_load_enviroment_variables()
     {
+        /** @var Client $client */
         $client = app(Client::class);
 
         $this->assertEquals('http://localhost:8080/client', $client->getEndpoint());
         $this->assertEquals('some-random-key', $client->getKey());
         $this->assertEquals('some-random-secret', $client->getSecret());
+    }
+
+    public function test_client_execute_commands()
+    {
+        Http::fake();
+
+        // The following test is created from example available in the following link (including the key and secret)
+        // http://docs.cloudstack.apache.org/en/latest/developersguide/dev.html#how-to-sign-an-api-call-with-python
+        config([
+            'services.cloudstack.endpoint' => 'http://localhost:8080/client/api',
+            'services.cloudstack.key' => 'plgWJfZK4gyS3mOMTVmjUVg-X-jlWlnfaUJ9GAbBbf9EdM-kAYMmAiLqzzq1ElZLYq_u38zCm0bewzGUdP66mg',
+            'services.cloudstack.secret' => 'VDaACYb0LV9eNjTetIOElcVQkvJck_J_QljX_FcHRj87ZKiy0z0ty0ZsYBkoXkY9b7eq1EhwJaw7FF3akA3KBQ',
+        ]);
+
+        /** @var Client $client */
+        $client = app(Client::class);
+
+        try {
+            $client->exec('listUsers');
+        } catch (RequestException $ex) {
+            $this->fail($ex->getMessage());
+        }
+
+        Http::assertSent(function (Request $request) {
+            return $request->url() === 'http://localhost:8080/client/api?'.
+                'apikey=plgWJfZK4gyS3mOMTVmjUVg-X-jlWlnfaUJ9GAbBbf9EdM-kAYMmAiLqzzq1ElZLYq_u38zCm0bewzGUdP66mg&'.
+                'command=listUsers&'.
+                'response=json&'.
+                'signature=TTpdDq%2F7j%2FJ58XCRHomKoQXEQds%3D';
+        });
     }
 }
